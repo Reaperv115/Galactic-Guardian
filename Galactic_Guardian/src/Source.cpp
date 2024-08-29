062f9357a23ff685110bd10e95f1dc86edbb70e5
Slaygony <rjs5785@hotmail.com>
2024-08-28 22:02:24 -0400

    learning rlgl

--- Galactic_Guardian/src/Source.cpp
+++ Galactic_Guardian/src/Source.cpp
@@ -25,6 +25,9 @@
 
 #include "raylib.h"
 #include "rlgl.h"
+#include "raymath.h"
+#include <numbers>
+#include <cmath>
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -36,6 +39,11 @@ int main(void)
 	const int screenWidth = 800;
 	const int screenHeight = 450;
 
+	double FoV = 90.0f * (std::numbers::pi / 180.000);
+	double aspectRatio = (double)screenWidth / (double)screenHeight;
+	double nearZ = 0.1;
+	double farZ = 100.0;
+
 	InitWindow(screenWidth, screenHeight, "Galactic Guardian");
 
 	Camera2D cam;
@@ -45,6 +53,11 @@ int main(void)
 	cam.zoom = 1.0f;
 
 	Rectangle player = { 400.0f, 280.0f, 40, 40 };
+
+	Matrix worldMatrix = MatrixIdentity();
+
+	// NOTE: fovy must be given in radians
+	Matrix viewMatrix = MatrixPerspective(FoV, aspectRatio, nearZ, farZ);
 
 	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 	//--------------------------------------------------------------------------------------
